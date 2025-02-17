810d0533420fb17ee46e29eff0a638816cfcb955
f.mirabdollahi@irfarabi.com <f.mirabdollahi@irfarabi.com>
2025-02-17 22:33:46 +0330

    Initial default layout

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -4,7 +4,7 @@ function DefaultLayout() {
   return (
     <div className="w-screen h-screen flex flex-col bg-background text-on-background">
       <header className="bg-primary text-on-primary p-4">
-        <h1 className="text-xl">Percentage Trader</h1>
+        <span className="text-xl font-bold">Percentage Trader</span>
       </header>
       <main className="flex-1 p-6">
         <div className="bg-primary-container p-4 rounded-lg shadow-md h-full">
