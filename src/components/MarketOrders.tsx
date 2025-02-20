3ab3c25b4259038f773ef6f14145b10ee4c2ec5f
f.mirabdollahi@irfarabi.com <f.mirabdollahi@irfarabi.com>
2025-02-20 21:50:05 +0330

    Add cumlative row for orders tables

--- src/components/MarketOrders.tsx
+++ src/components/MarketOrders.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, use } from "react";
 import { useParams } from "react-router-dom";
 import marketDetailsService from "../services/marketDetails.service";
 
@@ -8,6 +8,12 @@ interface MarketOrdersProps {
 
 function MarketOrders({ orderType }: MarketOrdersProps) {
   const [orders, setOrders] = useState<Order[] | null>(null);
+  const [cumulativeRow, setCumulativeRow] = useState({
+    price: 0,
+    value: 0,
+    remain: 0,
+  });
+
   const { marketId } = useParams();
 
   useEffect(() => {
@@ -19,6 +25,25 @@ function MarketOrders({ orderType }: MarketOrdersProps) {
     }
   }, [marketId, orderType]);
 
+  useEffect(() => {
+    if (orders) {
+      const sums = orders.reduce(
+        (acc, item) => {
+          acc.remain += Number(item.remain);
+          acc.value += Number(item.value);
+          acc.price += Number(item.price) * Number(item.remain);
+          return acc;
+        },
+        { price: 0, value: 0, remain: 0 }
+      );
+
+      setCumulativeRow({
+        price: sums.price / sums.remain,
+        value: sums.value,
+        remain: sums.remain,
+      });
+    }
+  }, [orders]);
   return (
     <div className="overflow-x-auto bg-background text-on-background p-4 rounded-lg shadow-md">
       <table className="w-full border-collapse">
@@ -41,6 +66,13 @@ function MarketOrders({ orderType }: MarketOrdersProps) {
                 <td className="py-2 px-4">{order.remain}</td>
               </tr>
             ))}
+          {orders && (
+            <tr className="bg-primary text-on-primary">
+              <td className="p-4">{cumulativeRow.price}</td>
+              <td className="p-4">{cumulativeRow.value}</td>
+              <td className="p-4">{cumulativeRow.remain}</td>
+            </tr>
+          )}
         </tbody>
       </table>
     </div>
