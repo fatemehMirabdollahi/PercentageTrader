3e544efcee1c5ba1d9f309f7e60fcb1caa44885b
f.mirabdollahi@irfarabi.com <f.mirabdollahi@irfarabi.com>
2025-02-21 01:50:23 +0330

    Add title of MarketDetail and back to MarketList options

--- src/pages/MarketDetails.tsx
+++ src/pages/MarketDetails.tsx
@@ -3,7 +3,17 @@ import { useState, useEffect } from "react";
 import Tabs from "../components/base/Tabs";
 import MarketTrades from "../components/MarketTrades";
 import MarketOrders from "../components/MarketOrders";
+import { ChevronLeftIcon } from "@heroicons/react/24/solid";
+/*************  ✨ Codeium Command ⭐  *************/
+/**
+ * MarketDetails component renders the market details page with tabbed navigation.
+ * It allows users to switch between different views: buy orders, sell orders, and trades.
+ * The component fetches the current market ID from URL parameters and manages the active tab state
+ * based on URL query parameters. It provides navigation between the tabs and displays
+ * the corresponding MarketTrades or MarketOrders components.
+ */
 
+/******  5ff30e97-5b35-499b-9b8d-567517ef87e4  *******/
 function MarketDetails() {
   const tabs = [
     { name: "سفارشات خرید", value: "buy" },
@@ -33,7 +43,12 @@ function MarketDetails() {
   };
 
   return (
-    <div className="p-4">
+    <div className="p-4 flex flex-col gap-4 ">
+      <div className="flex items-center gap-2" onClick={() => navigate(-1)}>
+        <span className="cursor-pointer">لیست رمزارزها</span>
+        <ChevronLeftIcon className="size-4" />
+        <span className="font-bold">اطلاعات بازار</span>
+      </div>
       <Tabs tabs={tabs} activeTab={activeTab} onTabChange={handleTabChange} />
       <div className="mt-4 w-full">
         {activeTab === "trades" && <MarketTrades />}
