6e66837d548564134b96fc004dbcc1b84827bd24
f.mirabdollahi@irfarabi.com <f.mirabdollahi@irfarabi.com>
2025-02-21 18:42:45 +0330

    Fix name of TradeForm and add error of validation toit

--- src/components/TradeForm.tsx
+++ src/components/TradeForm.tsx
@@ -6,39 +6,49 @@ interface TradeFormProps {
   ramainSum: number;
 }
 
-function Calculator({ orderBook, side, ramainSum }: TradeFormProps) {
+function TradeForm({ orderBook, side, ramainSum }: TradeFormProps) {
   const [inputValue, setInputValue] = useState("");
+  const [error, setError] = useState(false);
   const [requestResult, setRequestResult] = useState({
     value: 0,
     remain: 0,
     price: 0,
   });
+
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     const number = parseFloat(inputValue);
-    if (!isNaN(number)) {
-      let requestedVolume = ramainSum * (number / 100);
-      let remainingVolume = requestedVolume;
-      let sumValue = 0;
-      orderBook.forEach((order) => {
-        if (remainingVolume > order.remain) {
-          remainingVolume -= order.remain;
-          sumValue += order.price * order.remain;
-        } else {
-          sumValue += order.price * remainingVolume;
-          remainingVolume = 0;
-        }
-      });
-      setRequestResult({
-        value: sumValue,
-        remain: requestedVolume,
-        price: sumValue / requestedVolume,
-      });
+
+    if (isNaN(number) || number > 100 || number < 0) {
+      setError(true);
+      return;
     }
+
+    setError(false); // Reset error if input is valid
+
+    let requestedVolume = ramainSum * (number / 100);
+    let remainingVolume = requestedVolume;
+    let sumValue = 0;
+
+    orderBook.forEach((order) => {
+      if (remainingVolume > order.remain) {
+        remainingVolume -= order.remain;
+        sumValue += order.price * order.remain;
+      } else {
+        sumValue += order.price * remainingVolume;
+        remainingVolume = 0;
+      }
+    });
+
+    setRequestResult({
+      value: sumValue,
+      remain: requestedVolume,
+      price: sumValue / requestedVolume,
+    });
   };
 
   return (
-    <div className="  mt-10 p-6 shadow-lg rounded-lg">
+    <div className="mt-10 p-6 shadow-lg rounded-lg">
       <h2 className="text-lg font-semibold text-on-background mb-4">
         درصد حجم مورد نظر خود را برای {side === "sell" ? "خرید" : "فروش"} وارد
         کنید.
@@ -53,9 +63,16 @@ function Calculator({ orderBook, side, ramainSum }: TradeFormProps) {
             value={inputValue}
             required
             onChange={(e) => setInputValue(e.target.value)}
-            className="w-full sm:w-1/2 border border-secondary rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-primary"
+            className={`w-full sm:w-1/2 border ${
+              error
+                ? "border-negative focus:ring-negative"
+                : "border-secondary focus:ring-secondary"
+            } rounded-lg px-3 py-2 focus:outline-none focus:ring-2`}
           />
           <span className="mx-2">%</span>
+          {error && (
+            <span className="text-negative">درصد باید بین ۰ تا ۱۰۰ باشد.</span>
+          )}
         </div>
         <button
           type="submit"
@@ -81,7 +98,7 @@ function Calculator({ orderBook, side, ramainSum }: TradeFormProps) {
           <div className="text-lg font-semibold text-on-background">
             مبلغ قابل پرداخت:
             <span className="text-primary px-2">
-              {requestResult.price.toLocaleString()}
+              {requestResult.value.toLocaleString()}
             </span>
           </div>
         </div>
@@ -90,4 +107,4 @@ function Calculator({ orderBook, side, ramainSum }: TradeFormProps) {
   );
 }
 
-export default Calculator;
+export default TradeForm;
